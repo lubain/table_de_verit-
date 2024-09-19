d01812a2bc5f2ce50704bac725d5dd96f755222d
fadhel <fadhelubain@gmail.com>
2024-09-19 19:11:30 +0300

    optim

--- ia.py
+++ ia.py
@@ -11,20 +11,43 @@ def transposed(Mat):
 def determinant(Mat):
     return (Mat[0][0] * Mat[1][1]) - (Mat[0][1] * Mat[1][0])
 
+def isStr(Mat):
+    ligne = len(Mat)
+    colonne = len(Mat[0])
+    for i in range(ligne):
+        for j in range(colonne):
+            if type(Mat[i][j]) == str:
+                return True
+    return False
+
 def produit(A, B):
     ligne = len(A)
     colonne = len(B[0])
     c = len(B)
     p = []
-    for i in range(ligne):
-        temp = []
-        for j in range(colonne):
-            s = 0
-            for k in range(c):
-                s += A[i][k] * B[k][j]
-            temp.append(s)
-        p.append(temp)
-    print(p)
+    if isStr(A) or isStr(B):
+        for i in range(ligne):
+            temp = []
+            for j in range(colonne):
+                s = ""
+                for k in range(c):
+                    if k != 0:
+                        s = f"{s} + {A[i][k]}*{B[k][j]}"
+                    else:
+                        s = f"{A[i][k]}*{B[k][j]}"
+                temp.append(s)
+            p.append(temp)
+        print(p)
+    else:
+        for i in range(ligne):
+            temp = []
+            for j in range(colonne):
+                s = 0
+                for k in range(c):
+                    s += A[i][k] * B[k][j]
+                temp.append(s)
+            p.append(temp)
+        print(p)
     return p
 
 def variables(Mat):
@@ -35,7 +58,7 @@ def variables(Mat):
     print(v)
 
 def valeurPropre(Mat):
-    return (Mat[0][0] * Mat[1][1]) - (Mat[0][1] * Mat[1][0])
+    return
 
 if __name__ == "__main__":
     A = [
@@ -46,8 +69,7 @@ if __name__ == "__main__":
     B = [
         [1,1],
         [1,0],
-        [1,1]
+        [1,1],
     ]
-    
-    Mat = produit(transposed(B), transposed(A))
-    variables(Mat)
+    # Mat = produit(transposed(B), transposed(A))
+    Mat = produit(A, B)
